45d6ec6844db3c5609cd37dbda8d2f243fe3ddac
Axel Donath <axel.donath@cfa.harvard.edu>
2023-04-23 10:35:33 -0400

    Adjust signal noise plot

--- src/scripts/comparison-signal-noise.py
+++ src/scripts/comparison-signal-noise.py
@@ -1,4 +1,5 @@
 import config
+import matplotlib.lines as lines
 import numpy as np
 import paths
 from astropy.io import fits
@@ -32,29 +33,32 @@ scenario_titles = {
 instrument = "chandra"
 method = "jolideco-patch-prior-gleam-v0.1"
 
-bg_titles = {
-    "bg1": "Bkg 1",
-    "bg2": "Bkg 2",
-    "bg3": "Bkg 3",
-}
+bg_titles = [
+    ("bg1", "Data"),
+    ("bg1", "Reconstruction"),
+    ("bg2", "Data"),
+    ("bg2", "Reconstruction"),
+    ("bg3", "Data"),
+    ("bg3", "Reconstruction"),
+]
 
-figsize = config.FigureSizeAA(aspect_ratio=1.618, width_aa="two-column")
+figsize = config.FigureSizeAA(aspect_ratio=1.2, width_aa="two-column")
 
 upsampling_factor = 2
 DATA_SHAPE = (128, 128)
 
 gridspec_kw = {
-    "left": 0.08,
+    "left": 0.05,
     "right": 0.99,
-    "bottom": 0.02,
-    "top": 0.92,
+    "bottom": 0.01,
+    "top": 0.91,
     "wspace": 0.05,
     "hspace": 0.05,
 }
 
 fig, axes = plt.subplots(
-    nrows=len(bg_titles),
-    ncols=len(scenario_titles),
+    nrows=len(scenario_titles),
+    ncols=len(bg_titles),
     figsize=figsize.inch,
     gridspec_kw=gridspec_kw,
 )
@@ -102,12 +106,20 @@ def read_flux(scenario, bg, instrument):
     return data
 
 
+def move_axis(ax):
+    """Move axis"""
+    box = ax.get_position()
+    box.x0 = box.x0 - 0.007
+    box.x1 = box.x1 - 0.007
+    ax.set_position(box)
+
+
 for idx, scenario_title in enumerate(scenario_titles):
     scenario = scenario_titles[scenario_title]["name"]
     norm_kwargs = scenario_titles[scenario_title]["plot"]
 
-    for jdx, bg in enumerate(bg_titles):
-        if scenario == "data":
+    for jdx, (bg, bg_title) in enumerate(bg_titles):
+        if bg_title == "Data":
             data = read_and_stack_counts(scenario, bg, instrument)
         elif scenario == "gt":
             data = read_flux_ref(scenario)
@@ -116,22 +128,38 @@ for idx, scenario_title in enumerate(scenario_titles):
 
         norm_kwargs["max_cut"] = np.percentile(data, 99.9)
         norm = simple_norm(data, stretch="asinh", min_cut=0, **norm_kwargs)
-        ax = axes[jdx, idx]
+        ax = axes[idx, jdx]
         ax.imshow(data, cmap="viridis", origin="lower", norm=norm)
 
-        if bg == "bg1":
-            ax.set_title(scenario_title, fontsize=12)
+        if scenario == "spiral1":
+            ax.set_title(bg_title, fontsize=9)
 
         ax.set_axis_off()
 
-        if scenario == "spiral1":
+        if jdx == 0:
             ax.text(
-                x=-100,
-                y=DATA_SHAPE[0],
-                s=bg_titles[bg],
+                x=-37,
+                y=DATA_SHAPE[0] // 2.0,
+                s=scenario_title,
                 fontsize=12,
                 va="center",
             )
 
+        if (jdx + 1) % 2 == 0:
+            move_axis(ax)
+
+
+x_1, x_2 = 0.358, 0.674
+y_1, y_2 = 0.01, 0.96
+color = 3 * (0.5,)
+fig.add_artist(lines.Line2D([x_1, x_1], [y_1, y_2], color=color, lw=0.75))
+fig.add_artist(lines.Line2D([x_2, x_2], [y_1, y_2], color=color, lw=0.75))
+
+y = 0.97
+x_diff = 0.315
+x_0 = 0.21
+fig.text(x_0, y=y, s="Bkg 1", fontsize=12, ha="center", va="center")
+fig.text(x_0 + x_diff, y=y, s="Bkg 2", fontsize=12, ha="center", va="center")
+fig.text(x_0 + 2 * x_diff, y=y, s="Bkg 3", fontsize=12, ha="center", va="center")
 
 plt.savefig(paths.figures / "comparison-signal-noise.pdf", dpi=config.DPI)
