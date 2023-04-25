b7edef7aaa7bca5325a7c28015a77497b6d74e5a
Axel Donath <axel.donath@cfa.harvard.edu>
2023-04-25 16:56:13 -0400

    Update compare scenario plot

--- src/scripts/comparison-scenarios.py
+++ src/scripts/comparison-scenarios.py
@@ -6,22 +6,23 @@ from astropy.visualization import simple_norm
 from matplotlib import pyplot as plt
 from matplotlib.gridspec import GridSpec
 
+asinh_a = 0.01
 scenario_titles = {
     "A1": {
         "name": "point1",
-        "plot": {"max_cut": 340.0, "asinh_a": 0.02},
+        "plot": {"max_cut": 200.0, "asinh_a": asinh_a},
     },
     "B3": {
         "name": "aster3",
-        "plot": {"max_cut": 200.0, "asinh_a": 0.02},
+        "plot": {"max_cut": 200.0, "asinh_a": asinh_a},
     },
     "C3": {
         "name": "disk3",
-        "plot": {"max_cut": 200.0, "asinh_a": 0.02},
+        "plot": {"max_cut": 200.0, "asinh_a": asinh_a},
     },
-    "D3": {
-        "name": "spiral3",
-        "plot": {"max_cut": 100.0, "asinh_a": 0.02},
+    "D4": {
+        "name": "spiral4",
+        "plot": {"max_cut": 200.0, "asinh_a": asinh_a},
     },
 }
 
@@ -32,11 +33,11 @@ instrument = "chandra"
 method_titles = {
     "data": "Data",
     "gt": "Ground Truth",
-    "pylira": "Pylira",
     "jolideco-uniform-prior=n=10": "Jolideco\n(Uni, n=10)",
     "jolideco-uniform-prior=n=1000": "Jolideco\n(Unif., n=1000)",
-    "jolideco-patch-prior-gleam-v0.1": "Jolideco\n(GLEAM v0.1)",
+    "pylira": "Pylira",
     "jolideco-patch-prior-zoran-weiss": "Jolideco\n(Zoran-Weiss)",
+    "jolideco-patch-prior-gleam-v0.1": "Jolideco\n(GLEAM v0.1)",
 }
 
 figsize = config.FigureSizeAA(aspect_ratio=1.618, width_aa="two-column")
@@ -46,11 +47,11 @@ DATA_SHAPE = (128, 128)
 
 gridspec_kw = {
     "left": 0.05,
-    "right": 0.98,
+    "right": 0.90,
     "bottom": 0.02,
     "top": 0.92,
     "wspace": 0.05,
-    "hspace": 0.05,
+    "hspace": 0.02,
 }
 
 fig, axes = plt.subplots(
@@ -59,6 +60,18 @@ fig, axes = plt.subplots(
     figsize=figsize.inch,
     gridspec_kw=gridspec_kw,
 )
+
+
+def add_cbar(im, ax, fig, label=""):
+    """Add cbar to a given axis and figure"""
+    bbox = ax.get_position()
+    loright = bbox.corners()[-2]
+    rect = [loright[0] + 0.005, loright[1], 0.02, bbox.height]
+    cax = fig.add_axes(rect)
+    cbar = fig.colorbar(im, cax=cax, orientation="vertical")
+    cbar.set_label(label, size=9)
+    cbar.set_ticks([0, 10, 100])
+    cax.tick_params(labelsize=9)
 
 
 def read_and_stack_counts(scenario, bg, instrument):
@@ -116,21 +129,26 @@ for idx, scenario_title in enumerate(scenario_titles):
             data = read_flux(scenario, bg, instrument)
 
         norm = simple_norm(data, stretch="asinh", min_cut=0, **norm_kwargs)
-        axes[idx, jdx].imshow(data, cmap="viridis", origin="lower", norm=norm)
+
+        ax = axes[idx, jdx]
+        im = ax.imshow(data, cmap="viridis", origin="lower", norm=norm)
 
         if scenario == "point1":
-            axes[idx, jdx].set_title(method_titles[method], fontsize=9)
+            ax.set_title(method_titles[method], fontsize=9)
 
-        axes[idx, jdx].set_axis_off()
+        ax.set_axis_off()
 
         if method == "data":
-            axes[idx, jdx].text(
+            ax.text(
                 x=-40,
                 y=DATA_SHAPE[0] / 2.0,
                 s=scenario_title,
                 fontsize=12,
                 va="center",
             )
+
+        if jdx == len(method_titles) - 1:
+            add_cbar(im, ax, fig, label="Flux / A.U.")
 
 
 plt.savefig(paths.figures / "comparison-scenarios.pdf", dpi=config.DPI)
