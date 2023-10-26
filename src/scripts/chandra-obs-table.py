bc2f81b88c02fe3aebdaec6ce191dcb715c8fdf0
Axel Donath <axel.donath@cfa.harvard.edu>
2023-10-26 10:51:00 -0400

    Add hlines

--- src/scripts/chandra-obs-table.py
+++ src/scripts/chandra-obs-table.py
@@ -1,6 +1,7 @@
 import logging
 from io import StringIO
 
+import numpy as np
 import paths
 from astropy import units as u
 from astropy.table import Table
@@ -11,7 +12,7 @@ logging.basicConfig(level=logging.INFO)
 log = logging.getLogger(__name__)
 OBS_ID_REF = 8365
 
-colnames = ["Obs ID", "Exposure", "Start Date"]
+colnames = ["Obs ID", "Exposure", "Obs Date"]
 obs_table = Table(names=colnames, dtype=[int, "f8", "S10"], units=["", "ks", ""])
 
 path = paths.jolideco_repo_chandra_example / "data"
@@ -26,12 +27,20 @@ for filename in filenames:
     obs_table.add_row(row)
 
 
+# Add reference observation
+obs_table.add_index("Obs ID")
+exposure_ref = obs_table.loc[OBS_ID_REF]["Exposure"]
+obs_table["Rel. Exposure"] = np.round(obs_table["Exposure"] / exposure_ref, 1)
+
 content_io = StringIO()
 obs_table.write(content_io, format="latex", overwrite=True)
 
 content = content_io.getvalue()
 lines = content.split("\n")
 lines = lines[2:-3]
+lines.insert(1, "\hline")
+lines.insert(3, "\hline")
+lines.append("\hline")
 content = "\n".join(lines)
 
 filename = paths.output / "chandra-obs-table.tex"
